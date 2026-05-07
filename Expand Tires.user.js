b9111960da789d8a96e1b35667949de67c7cc803
cameronarnoldbbt <cameron.arnold@bigbrandtire.com>
2026-05-07 07:19:19 -0700

    Update Expand Tires.user.js

--- Expand Tires.user.js
+++ Expand Tires.user.js
@@ -2,8 +2,8 @@
 // @name         Expand Tires
 // @namespace    http://tampermonkey.net/
 // @version      2.0
-// @updateURL    https://githubusercontent.com
-// @downloadURL  https://githubusercontent.com
+// @updateURL    https://github.com/cameronarnoldbbt/BBTAutomation/raw/refs/heads/main/Expand%20Tires.user.js
+// @downloadURL  https://github.com/cameronarnoldbbt/BBTAutomation/raw/refs/heads/main/Expand%20Tires.user.js
 // @match        https://edge.bigbrandtire.com/POS/Tire/*
 // @grant        none
 // @run-at       document-idle
